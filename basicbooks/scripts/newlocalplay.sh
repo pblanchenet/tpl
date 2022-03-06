f758e48e7491db09a185ee6143c4957a39d24cd2
pierre-marie blanchenet <pblanchenet@gmail.com>
2022-03-06 14:19:16 +0100

    Init: master

---
+++ basicbooks/scripts/newlocalplay.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#----------------------------------------------
+# Author: Pierre-Marie BLANCHENET
+# DATE: March 2022
+#----------------------------------------------
+
+lplays=$*
+
+cd ..
+
+createLocal()
+{
+  play=$1
+  ansible-playbook newlocalplay.yml -e "tname=${play}"
+}
+
+main()
+{
+  for lplay in ${lplays}
+  do
+    createLocal ${lplay}
+  done
+}
+
+main 2>&1 
